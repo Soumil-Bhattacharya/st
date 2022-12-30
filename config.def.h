dc06817d279456bdd732f227e209d2c14f3256e9
Soumil-Bhattacharya <soumilbhattacharya.sb@gmail.com>
2022-12-30 16:08:07 +0530

    Change Font Size for dwm style window arrangement

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Fira Code:style=Retina:pixelsize=20:antialias=true:autohint=true";
+static char *font = "Fira Code:style=Retina:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
